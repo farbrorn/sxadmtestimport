6c873ecd7df1fcc2374adb28bcb606fa57a867b9
ulf.hemma@gmail.com <ulf.hemma@gmail.com@5eed21e7-064c-0410-8c3d-51462c4d707e>
2012-03-18 17:15:31 +0000

    SxInfoException

--- SxLibrary/src/se/saljex/sxlibrary/SxServerMainRemote.java
+++ SxLibrary/src/se/saljex/sxlibrary/SxServerMainRemote.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javax.ejb.Remote;
 import se.saljex.sxlibrary.exceptions.SXEntityNotFoundException;
+import se.saljex.sxlibrary.exceptions.SxInfoException;
 
 /**
  *
@@ -20,9 +21,9 @@ import se.saljex.sxlibrary.exceptions.SXEntityNotFoundException;
 public interface SxServerMainRemote {
     public ByteArrayOutputStream getPdfFaktura(int faktnr) throws IOException;
 	String getHTMLStatus();
-	ArrayList<Integer> saveSxShopOrder(int kontaktId, String kundnr, String kontaktNamn, short lagerNr, String marke) throws KreditSparrException;
+	ArrayList<Integer> saveSxShopOrder(int kontaktId, String kundnr, String kontaktNamn, short lagerNr, String marke) throws KreditSparrException, SxInfoException;
 	boolean sendSimpleMail(String adress, String header, String bodytext);
-	int overforBVOrder(String sxKundnr, int bvOrdernr, String bvAnvandare, String sxAnvandare, short sxLagernr) throws SXEntityNotFoundException;
+	int overforBVOrder(String sxKundnr, int bvOrdernr, String bvAnvandare, String sxAnvandare, short sxLagernr) throws SXEntityNotFoundException, SxInfoException;
 //	int overforOrder(String mainKundnr, int localOrdernr, String localAnvandare, String mainAnvandare, short mainLagernr) throws SXEntityNotFoundException;
 
 	public String getSXReg(String nyckel, String defaultVarde);
---
+++ SxLibrary/src/se/saljex/sxlibrary/exceptions/SxInfoException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.saljex.sxlibrary.exceptions;
+
+/**
+ *
+ * @author Ulf
+ */
+public class SxInfoException extends java.lang.Exception {
+	public SxInfoException(String s) { super(s); }
+}
